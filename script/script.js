495fff556f4f637ae6d188a187c957d88f44f71f
JosanaKla <josana.0205@gmail.com>
2022-06-06 20:58:45 -0300

    adicao da barra inferior que esqueci e inicio da adicao dos comentarios

---
+++ script/script.js
@@ -0,0 +1,10 @@
+/*
+function postarDados(path, name) {
+  document.getElementById("comentario").name  = name;
+ 
+ document.getElementById("form").action = path;
+ document.getElementById("form").submit();
+}
+
+postarDados("legal");
+ */
